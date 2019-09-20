d74ad82c118a4b4c3b87e5aced8b52a106abfb2b
Robert <robertgolosynsky@gmail.com>
2019-09-20 22:05:22 +0200

    get coverage of a module

--- coverage_metrics/coverage_interface.py
+++ coverage_metrics/coverage_interface.py
@@ -1,4 +1,7 @@
 import abc
+import itertools
+
+from coverage_metrics.util import percent
 
 
 class Coverage(abc.ABC):
@@ -8,3 +11,13 @@ class Coverage(abc.ABC):
 
     def covered_items_of(self, module_path, of_type=None, selected_node_ids=None) -> dict:
         raise NotImplementedError("Implement this")
+
+    def coverage_of(self, module_path, of_type=None, selected_node_ids=None) -> int:
+        covered_items_per_node_id = self.covered_items_of(
+            module_path,
+            of_type=of_type,
+            selected_node_ids=selected_node_ids
+        )
+        total_covered_items = set(itertools.chain.from_iterable(covered_items_per_node_id.values()))
+        total_possible_items = self.total_items_of(module_path, of_type=of_type)
+        return int(percent(total_covered_items, total_possible_items))
